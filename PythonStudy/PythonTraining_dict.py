007fc044c859e4bc54bc66a67f155ad93f696ee2
zzr999zzr <zzr_999@sina.com>
2018-04-09 17:38:50 +0800

    1.添加字典练习

---
+++ PythonStudy/PythonTraining_dict.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+#!-*- coding:utf-8 -*-
+#Author:Alfa
+#Time   : 2018/4/9 15:01
+
+"""
+字典dictionary练习
+增删改查
+"""
+
+#创建字典
+dict1 = {'test': 3, '缓缓': 'yyyy', 1: 'gggg'}
+print(dict1)  #{'test': 3, '缓缓': 'yyyy', 1: 'gggg'}
+
+dict2 = dict((('a', 'b'),))
+print(dict2)  #{'a': 'b'},当只有一个键值对时，最后要添加一个逗号“，”
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
